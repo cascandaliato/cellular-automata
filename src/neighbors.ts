a7ad25a2adef2f117f472074c352c256f7a8bfe3
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2021-01-10 12:00:27 +0100

    document neighborhood functions

--- src/neighbors.ts
+++ src/neighbors.ts
@@ -8,17 +8,29 @@ const getNeighborsFromDirections = <DIMENSIONS extends number>(
 ) => (coords: Coords<DIMENSIONS>): Coords<DIMENSIONS>[] =>
   directions.map(d => coords.map((n, idx) => n + d[idx]) as Coords<DIMENSIONS>);
 
-const mooreDirections = (n: number): TupleOf<typeof n, number>[] =>
-  product([0, 1, -1], n).filter(p => p.some(v => v !== 0));
+const mooreDirections = (dimensions: number): Coords<typeof dimensions>[] =>
+  product([0, 1, -1], dimensions).filter(p => p.some(v => v !== 0));
 
+/**
+ *  [Moore neighborhood](https://en.wikipedia.org/wiki/Moore_neighborhood):
+ *```
+ * 1D:               2D:
+ *  0 1 2 3 4           0 1 2 3 4
+ *    X O X           0
+ *                    1   X X X
+ *                    2   X O X
+ *                    3   X X X
+ *                    4
+ * ```
+ */
 const moore = (dimensions: number) =>
-  getNeighborsFromDirections<typeof dimensions>(mooreDirections(dimensions));
+  getNeighborsFromDirections(mooreDirections(dimensions));
 
 const vonNeumannDirections = (
   dimensions: number,
   distance: number,
   residue: number = distance
-): TupleOf<typeof dimensions, number>[] => {
+): Coords<typeof dimensions>[] => {
   if (dimensions === 0) return residue === 0 ? [[]] : [];
 
   return range(residue !== 0 ? -residue : 0, residue + 1).flatMap(r =>
@@ -30,13 +42,45 @@ const vonNeumannDirections = (
   );
 };
 
-// https://en.wikipedia.org/wiki/Von_Neumann_neighborhood
+/**
+ *  [Von Neumann neighborhood](https://en.wikipedia.org/wiki/Von_Neumann_neighborhood):
+ *```
+ * Distance 1:      Distance 2:
+ *    0 1 2            0 1 2 3 4
+ * 0   X            0     X
+ * 1 X O X          1   X X X
+ * 2   X            2 X X O X X
+ *                   3   X X X
+ *                   4     X
+ * ```
+ */
 const vonNeumann = (dimensions: number, distance: number = 1) =>
   getNeighborsFromDirections<typeof dimensions>(
     range(1, distance + 1).flatMap(d => vonNeumannDirections(dimensions, d))
   );
 
+const hexGridDirections: [number, number][] = [
+  [0, 1], // east
+  [1, 0], // south-east
+  [1, -1], // south-west
+  [0, -1], // west
+  [-1, 0], // north-west
+  [-1, 1], // north-east
+];
+
+/**
+ * Hex cell orientation and directions:
+ * ```
+ *                 0  1  2
+ *  nw /\ ne    0     nw ne
+ *  w |  | e    1  w  O  e
+ *  sw \/ se    2 sw  se
+ * ```
+ */
+const hexGrid2D = () => getNeighborsFromDirections<2>(hexGridDirections);
+
 export {
+  hexGrid2D,
   moore,
   moore as surrounding,
   vonNeumann,
--- test/conway.test.ts
+++ test/conway.test.ts
@@ -1,4 +1,4 @@
-import { vonNeumann } from '../src/neighbors';
+import { hexGrid2D, moore, vonNeumann } from '../src/neighbors';
 import { Conway } from '../src/presets';
 
 describe("Conway's Game of Life", () => {
@@ -15,25 +15,37 @@ describe("Conway's Game of Life", () => {
 });
 
 describe('Neighbors', () => {
-  test("VonNeumann's neighborhood (1d, distance 1)", () => {
+  test('Moore neighborhood (1d)', () => {
+    expect(moore(1)([1])).toMatchSnapshot();
+  });
+
+  test('Moore neighborhood (2d)', () => {
+    expect(moore(2)([1, 2])).toMatchSnapshot();
+  });
+
+  test('VonNeumann neighborhood (1d, distance 1)', () => {
     expect(vonNeumann(1, 1)([1])).toMatchSnapshot();
   });
 
-  test("VonNeumann's neighborhood (1d, distance 2)", () => {
+  test('VonNeumann neighborhood (1d, distance 2)', () => {
     expect(vonNeumann(1, 2)([1])).toMatchSnapshot();
   });
 
-  test("VonNeumann's neighborhood (2d, distance 1)", () => {
+  test('VonNeumann neighborhood (2d, distance 1)', () => {
     expect(vonNeumann(2, 2)([1, 1])).toMatchSnapshot();
   });
-  test("VonNeumann's neighborhood (2d, distance 2)", () => {
+  test('VonNeumann neighborhood (2d, distance 2)', () => {
     expect(vonNeumann(2, 2)([1, 1])).toMatchSnapshot();
   });
 
-  test("VonNeumann's neighborhood (3d, distance 1)", () => {
+  test('VonNeumann neighborhood (3d, distance 1)', () => {
     expect(vonNeumann(3, 1)([1, 1, 0])).toMatchSnapshot();
   });
-  test("VonNeumann's neighborhood (3d, distance 2)", () => {
+  test('VonNeumann neighborhood (3d, distance 2)', () => {
     expect(vonNeumann(3, 2)([1, 1, 0])).toMatchSnapshot();
+  });
+
+  test('Hex grid neighbors of [1,2]', () => {
+    expect(hexGrid2D()([1, 2])).toMatchSnapshot();
   });
 });
